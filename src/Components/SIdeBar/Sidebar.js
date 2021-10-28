41b573727def9ed9b4892673598f9203b6eb0353
Draqode <Boomjay008@gmail.com>
2021-10-28 08:56:55 +0100

    image loading fixed moving on with dashboard routing and loan logic implementation

--- src/Components/SIdeBar/Sidebar.js
+++ src/Components/SIdeBar/Sidebar.js
@@ -6,7 +6,9 @@ import './Sidebar.scss'
 import logout from '../../bg-images/logout_black_24dp.svg'
 
 const Sidebar = () => {
+    // sets users name
     const [userName, setUserName] = useState('')
+    // Sets user profile photo
     const [userImage, setUserImage] = useState('')
     onAuthStateChanged(auth, (user) => {
         if (user) {
