c39f4e590afb1087483d1132528c63a821276979
Draqode <Boomjay008@gmail.com>
2021-11-09 12:37:22 +0100

    new changes

--- src/Components/Table/Table.js
+++ src/Components/Table/Table.js
@@ -5,10 +5,10 @@ const Table = ({id, symbol, pps, Qt, eQv}) => {
     return (
         <div className = 'table-row'>
             <h4> { id }</h4>
-            <h4 className = 'sym'> {symbol }</h4>
-            <h4 className = 'pps'> {pps }</h4>
+            <h4 className = 'sym' style = {{marginLeft: '-4.5rem'}}> {symbol }</h4>
+            <h4 className = 'pps' style = {{marginLeft: '-1.5rem'}}> {pps }</h4>
             <h4> {Qt} </h4>
-            <h4 className = 'eqv'> {eQv} </h4>
+            <h4 className = 'eqv' style = {{marginRight: '3rem'}}> {eQv} </h4>
         </div>
     )
 }
